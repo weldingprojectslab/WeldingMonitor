8ad16a9f12bfe8a8a56575f8a6fd719d7d2ad05e
welding projectslab <weldingprojectslab@gmail.com>
2016-10-18 22:46:33 +0300

    basic qml charts initialisian codes and files added..

--- WeldingMonitor/main.cpp
+++ WeldingMonitor/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    engine.addImportPath("../WeldingMonitor/qml");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
